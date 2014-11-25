ec250374721ff393ab237ae82c9ed4fb4298c983
Christopher Hiller <chiller@badwing.com>
2014-11-24 23:12:20 -0800

    interim commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,65 @@
 'use strict';
 
+var glob = require('glob').sync,
+  extend = require('extend'),
+  loadGruntConfig = require('load-grunt-config'),
+  path = require('path'),
+  join = path.join,
+  basename = path.basename,
+  dirname = path.dirname,
+  slugFn = require('slug'),
+
+  slug = function slug() {
+    return slugFn.apply(null, Array.prototype.slice.call(arguments).concat('_'));
+  },
+
+  dist = join.bind(null, 'dist');
+
+
 module.exports = function (grunt) {
 
-  grunt.registerMultiTask('write', 'Writes contents to a file', function () {
-    var key;
-    for (key in this.data) {
-      if (this.data.hasOwnProperty(key)) {
-        grunt.file.write(key, this.data[key]);
-        grunt.log.ok('Wrote %d bytes to %s', this.data[key].length, key);
-      }
-    }
-  });
-
-  require('load-grunt-config')(grunt, {
-    configPath: 'tasks',
-    data: {
-      autoLoadImages: {
-        pkg: require('./lib/wanted/auto-load-images/bower.json')
+  var readJSON = grunt.file.readJSON,
+    root_opts = {
+      configPath: 'tasks',
+      data: {
+        namespace: 'org.badwing',
+        pkg: readJSON('package.json')
       },
-      namespace: 'http://badwing.com/',
-      pkg: require('./package.json')
-    }
+      init: false
+    },
+    task_dirs = {},
+    config;
+
+  glob('./lib/**/bower.json').forEach(function (bower_path) {
+    var bower = readJSON(bower_path),
+      base = basename(bower.main, '.js'),
+      name = slug(base),
+      user_path = dist(base + '.user.js'),
+      meta_path = dist(base + '.meta.js');
+
+    root_opts.data[name] = {
+      bower: bower,
+      user_path: user_path,
+      user: basename(user_path),
+      meta_path: meta_path,
+      meta: basename(meta_path),
+      main_path: join(dirname(bower_path), bower.main)
+    };
   });
 
+  config = loadGruntConfig(grunt, root_opts);
+
+  glob('./lib/**/tasks/').forEach(function (group) {
+    task_dirs[group] = true;
+  });
+
+  Object.keys(task_dirs).forEach(function (group) {
+    var opts = extend({}, root_opts, {
+      configPath: group
+    });
+    extend(config, loadGruntConfig(grunt, opts));
+  });
+
+
+  grunt.initConfig(config);
 };
--- dist/auto-load-images.meta.js
+++ dist/auto-load-images.meta.js
@@ -1,13 +1,20 @@
 // ==UserScript==
-// @name         BrickLink - Wanted List: Automatically Load Images
-// @namespace    http://badwing.com/
-// @homepage     https://github.com/boneskull/bricklink-scripts
-// @version      0.1.2
-// @description  Automatically load images when adding items to wanted list
-// @author       Christopher Hiller <chiller@badwing.com>
-// @downloadURL  https://raw.githubusercontent.com/boneskull/bricklink-scripts/master/dist/auto-load-images.js
-// @updateURL    https://raw.githubusercontent.com/boneskull/bricklink-scripts/master/dist/auto-load-images.meta.js
-// @include      http://www.bricklink.com/wantedAddDetail.asp?*
-// @require      //code.jquery.com/jquery-1.11.1.min.js
-// @require      //cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/jquery.spin.min.js
+// @name        BrickLink - Wanted List: Automatically Load Images
+// @namespace   org.badwing
+// @homepage    https://github.com/boneskull/bricklink-scripts
+// @version     0.1.2
+// @description Automatically load images when adding items to wanted list
+// @author      Christopher Hiller <chiller@badwing.com>
+// @downloadURL https://raw.githubusercontent.com/boneskull/bricklink-scripts/master/dist/auto-load-images.user.js
+// @updateURL   https://raw.githubusercontent.com/boneskull/bricklink-scripts/master/dist/auto-load-images.meta.js
+// @include     http://www.bricklink.com/wantedAddDetail.asp?*
+// @require     http://code.jquery.com/jquery-1.11.1.min.js
+// @require     http://cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js
+// @require     http://cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/jquery.spin.min.js
+// @require     http://cdnjs.cloudflare.com/ajax/libs/jquery-throttle-debounce/1.1/jquery.ba-throttle-debounce.min.js
+// @require     https://greasyfork.org/scripts/6649-transparency/code/transparency.js?version=25968
+// @resource    template https://raw.githubusercontent.com/boneskull/bricklink-scripts/master/dist/auto-load-images.html
+// @license     MIT
+// @grant       GM_getResourceText
 // ==/UserScript==
+
